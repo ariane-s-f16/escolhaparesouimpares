dfbf6a6f2d8595d89c25340afcb90f019ad6aec3
Usuario <Usuario@CPD03-M11>
2024-03-26 11:07:03 -0300

    Adicionar arquivos de projeto.

---
+++ escolhaparesouimpares/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace escolhaparesouimpares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("escola entre pares ou impares(1=i e 0=p)");
+           int opc= int.Parse(Console.ReadLine());
+            int cont = opc;
+
+            while (cont <= 20) 
+            {
+            Console.WriteLine(cont);
+                cont += 2; 
+            }
+            Console.ReadKey();
+        }
+    }
+}
